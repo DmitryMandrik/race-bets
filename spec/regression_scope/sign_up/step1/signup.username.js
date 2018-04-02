8862a6e85a9bc9932b10c7fc7701b31aac2efa19
DmitryMandrik <Dmitrymandrik@coherentsolutions.com>
2018-04-02 12:25:34 +0300

    Update signup.username.js

--- spec/regression_scope/sign_up/step1/signup.username.js
+++ spec/regression_scope/sign_up/step1/signup.username.js
@@ -21,37 +21,37 @@ describe('SignUp Form regression_scope - UserName (1 step))', function () {
             browser.wait(EC.textToBePresentInElement(signUpPage.activeStep, 'Personal details'), 10000);
         });
 
-        // it('invalid when username is short than 6', async function () {
-        //     let tooShortName = randomstring.generate(5);
-        //     signUpPage.setUserName(await tooShortName);
-        //     signUpPage.submitSignUp();
-        //     expect(signUpPage.getError()).toEqual('At least 6 characters');
-        // });
-        //
-        // it('invalid when username is empty', function () {
-        //     signUpPage.setUserName('');
-        //     signUpPage.submitSignUp();
-        //     expect(signUpPage.getError()).toEqual('This field is required')
-        // });
-        //
-        // it('invalid when username is incorrect format', async function () {
-        //     let incorrectFormatName = randomstring.generate(5) + "./,+=?[]!@#'$%^~`&*(){}".split('')[(Math.floor(Math.random()*20))];
-        //     signUpPage.setUserName(await incorrectFormatName);
-        //     signUpPage.submitSignUp();
-        //     expect(signUpPage.getError()).toEqual('Only A-Z, 0-9, - and _')
-        // });
-        //
-        // it('invalid when username is too long', async function () {
-        //     let tooLong = randomstring.generate(21);
-        //     signUpPage.setUserName(await tooLong);
-        //     signUpPage.submitSignUp();
-        //     expect(signUpPage.getError()).toEqual('Maximum of 20 characters')
-        // });
-        //
-        // it('invalid when username already registered', function () {
-        //     signUpPage.setUserName('john99');
-        //     signUpPage.submitSignUp();
-        //     expect(signUpPage.getError()).toEqual('This username has already been taken')
-        // });
+        it('invalid when username is short than 6', async function () {
+            let tooShortName = randomstring.generate(5);
+            signUpPage.setUserName(await tooShortName);
+            signUpPage.submitSignUp();
+            expect(signUpPage.getError()).toEqual('At least 6 characters');
+        });
+        
+        it('invalid when username is empty', function () {
+            signUpPage.setUserName('');
+            signUpPage.submitSignUp();
+            expect(signUpPage.getError()).toEqual('This field is required')
+        });
+        
+        it('invalid when username is incorrect format', async function () {
+            let incorrectFormatName = randomstring.generate(5) + "./,+=?[]!@#'$%^~`&*(){}".split('')[(Math.floor(Math.random()*20))];
+            signUpPage.setUserName(await incorrectFormatName);
+            signUpPage.submitSignUp();
+            expect(signUpPage.getError()).toEqual('Only A-Z, 0-9, - and _')
+        });
+        
+        it('invalid when username is too long', async function () {
+            let tooLong = randomstring.generate(21);
+            signUpPage.setUserName(await tooLong);
+            signUpPage.submitSignUp();
+            expect(signUpPage.getError()).toEqual('Maximum of 20 characters')
+        });
+        
+        it('invalid when username already registered', function () {
+            signUpPage.setUserName('john99');
+            signUpPage.submitSignUp();
+            expect(signUpPage.getError()).toEqual('This username has already been taken')
+        });
     });
 });
